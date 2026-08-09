d7588e88cbdca3729c82dd4c65c575f6bb1571b2
Yasminie Sahadeo <yasminiesahadeo@gmail.com>
2026-08-08 20:24:52 -0400

    Initial setup

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  allowedDevOrigins: ['10.0.0.200'],
+}
+
+export default nextConfig;
